18882ee2a93bd4099d5f8e9d04c9a38d2f5bd557
Elie Bissor <eliebiss.26@gmail.com>
2023-11-22 15:22:39 +0100

    Add search context

---
+++ dictionary-app/src/app/_contexts/SearchContext/index.tsx
@@ -0,0 +1,38 @@
+"use client";
+import { api } from "@/app/_services/api";
+import { createContext, ReactNode, useContext, useState } from "react";
+
+interface SearchProviderProps {
+  children: ReactNode;
+}
+
+interface SearchContext {
+  handleSearchData: any;
+  result: any;
+}
+
+const SearchContext = createContext({} as SearchContext);
+
+export const useSearch = () => {
+  return useContext(SearchContext);
+};
+
+export const SearchProvider = ({ children }: SearchProviderProps) => {
+  const [result, setResult] = useState<any>("keyboard");
+
+  const handleSearchData = async (word: string) => {
+    const data = await api.fetchWords(word);
+    setResult(data);
+  };
+
+  return (
+    <SearchContext.Provider
+      value={{
+        handleSearchData,
+        result,
+      }}
+    >
+      {children}
+    </SearchContext.Provider>
+  );
+};
