6de6fe6f8d2e44ee7a6efb015160dcae6bf00fc7
Elie Bissor <eliebiss.26@gmail.com>
2024-01-24 14:34:24 +0100

    Update content of NotFOundError component.

--- dictionary-app/src/app/_components/molecules/NotFoundError/index.tsx
+++ dictionary-app/src/app/_components/molecules/NotFoundError/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import "@/app/_styles/components/molecules/_not-found-error.scss";
 
 interface NotFoundErrorProps {
@@ -7,7 +8,13 @@ interface NotFoundErrorProps {
 export const NotFoundError = (props: NotFoundErrorProps) => {
   return (
     <div className="__not_found_error">
-      <h1>No results found for {props.word}</h1>
+      <span>😕</span>
+      <h3>No results found for {props.word}</h3>
+      <p>
+        Sorry pal, we couldn't find definitions for the word you were looking
+        for. You can try the search again at later time or head to the web
+        instead.
+      </p>
     </div>
   );
 };
